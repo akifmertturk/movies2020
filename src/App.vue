4c88dd01eaa0c362c881873d3523e4fe634a715b
akifmertturk <fikafi08@gmail.com>
2021-03-28 21:46:44 +0300

    bug fix, when double clicked on the empty search box, items was shown as [object Object]

--- src/App.vue
+++ src/App.vue
@@ -15,6 +15,7 @@
         hide-no-data
         hide-selected
         item-text="Title"
+        item-value="Title"
         label="Movies in 2020"
         loader-height="10"
         placeholder="Start typing to Search"
@@ -31,7 +32,7 @@
         <v-row align="center">
           <v-col cols="4" sm="1" centered>
             <v-img 
-            :src="poster" alt="image" max-width="200" max-height="200" class="ml-4"></v-img>
+            :src="poster" alt="poster" max-width="200" max-height="200" class="ml-4"></v-img>
           </v-col>
           <v-col cols="12" sm="4">
             <v-list-item
@@ -66,14 +67,20 @@ export default {
     isLoading: false,
     model: null,
     search: null,
-    poster: '',
+    posterUrl: ''
   }),
 
   computed: {
+    poster () {
+      if(!this.model) return ''
+      if(this.entries === []) return ''
+      return this.posterUrl
+    },
     fields () {
       console.log(this.model);
 
       if (!this.model) return []
+
       return Object.keys(this.model).map(key => {
         console.log('key is :' + this.model[key]);
         return {
@@ -110,8 +117,17 @@ export default {
           this.result.imdbRating = res.imdbRating
           this.result.Year = res.Year
           this.result.Genre = res.Genre
-          this.poster = res.Poster
-          this.entries.push(this.result)
+          this.posterUrl = res.Poster
+          if(this.entries.length < 1){
+            // user types for a movie first time, fill the array with the result of the query
+            this.entries.push(this.result)
+          }
+          else if(typeof this.entries[0].Title === 'undefined') {
+            // user deleted the search box, so clear the array
+            this.entries = []
+          }
+          // user uses the same item in the search box, do nothing
+          console.log('entries len: ' + this.entries.length);
         })
         .catch(err => {
           console.log(err)
